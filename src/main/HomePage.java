37a3d0c9ea248139e68de6bea623cc9b71b64482
Omer Melih Oner <omermelihoner1@gmail.com>
2021-05-15 13:46:59 -0400

    added buttons to home page.

--- src/main/HomePage.java
+++ src/main/HomePage.java
@@ -9,10 +9,12 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
+import javax.swing.JPanel;
 
 public class HomePage {
 
 	private JFrame frame;
+	private JTextField txtAcasdfasd;
 
 	/**
 	 * Launch the application.
@@ -46,12 +48,34 @@ public class HomePage {
 		frame.getContentPane().setLayout(null);
 		ImageIcon img = new ImageIcon("images/background.png");
 		
+		JButton btnNewButton = new JButton("Inventory");
+		btnNewButton.setToolTipText("");
+		btnNewButton.setBounds(380, 245, 130, 30);
+		frame.getContentPane().add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Explore");
+		btnNewButton_1.setBounds(380, 215, 130, 30);
+		frame.getContentPane().add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("Move");
+		btnNewButton_2.setForeground(new Color(124, 252, 0));
+		btnNewButton_2.setBackground(new Color(169, 169, 169));
+		btnNewButton_2.setBounds(380, 185, 130, 30);
+		frame.getContentPane().add(btnNewButton_2);
+		btnNewButton_2.setOpaque(true);
+		
+		JButton btnNewButton_3 = new JButton("Sleep");
+		btnNewButton_3.setBounds(255, 245, 130, 30);
+		frame.getContentPane().add(btnNewButton_3);
 		
 		JLabel lblNewLabel = new JLabel("",img, JLabel.CENTER);
-		lblNewLabel.setBounds(0, 0, 500, 320);
+		lblNewLabel.setBounds(0, 0, 510, 320);
 		frame.getContentPane().add(lblNewLabel);
+		
+		
+
 		frame.setBounds(100, 100, 500, 320);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 	}
-
 }
