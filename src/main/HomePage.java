1a6b79df23e650839b14465d0db81f2342393723
Omer Melih Oner <omermelihoner1@gmail.com>
2021-05-17 19:02:16 -0400

    no message

--- src/main/HomePage.java
+++ src/main/HomePage.java
@@ -51,22 +51,19 @@ public class HomePage {
 		
 		JButton btnNewButton = new JButton("Inventory");
 		btnNewButton.setToolTipText("");
-		btnNewButton.setBounds(380, 245, 130, 30);
+		btnNewButton.setBounds(370, 245, 130, 30);
 		frame.getContentPane().add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("Explore");
-		btnNewButton_1.setBounds(380, 215, 130, 30);
+		btnNewButton_1.setBounds(370, 215, 130, 30);
 		frame.getContentPane().add(btnNewButton_1);
 		
 		JButton btnNewButton_2 = new JButton("Move");
-		btnNewButton_2.setForeground(new Color(124, 252, 0));
-		btnNewButton_2.setBackground(new Color(169, 169, 169));
-		btnNewButton_2.setBounds(380, 185, 130, 30);
+		btnNewButton_2.setBounds(370, 185, 130, 30);
 		frame.getContentPane().add(btnNewButton_2);
-		btnNewButton_2.setOpaque(true);
 		
 		JButton btnNewButton_3 = new JButton("Sleep");
-		btnNewButton_3.setBounds(255, 245, 130, 30);
+		btnNewButton_3.setBounds(245, 245, 130, 30);
 		frame.getContentPane().add(btnNewButton_3);
 		
 		JLabel lblNewLabel = new JLabel("",img, JLabel.CENTER);
@@ -74,7 +71,7 @@ public class HomePage {
 		frame.getContentPane().add(lblNewLabel);
 		
 		JProgressBar progressBar = new JProgressBar();
-		progressBar.setBounds(0, 154, 146, 20);
+		progressBar.setBounds(0, 155, 145, 20);
 		frame.getContentPane().add(progressBar);
 		
 		
